0103beeb781fb4ae333f5658682c787a194f8b6a
Kali <74075397+Kalitsune@users.noreply.github.com>
2022-05-16 15:19:55 +0200

    fixing server private key generation

--- server/config/properties.go
+++ server/config/properties.go
@@ -15,22 +15,24 @@ func GetPrivateKey() string {
 }
 
 func GeneratePrivateKey() {
-	//generate random characters and set it as private key
-	var privateKey string
-	for i := 0; i < 32; i++ {
-		privateKey += string(rune(65 + rand.Intn(25)))
+	//generate a private key using crypto/rand
+	privateKey := make([]byte, 32)
+	_, err := rand.Read(privateKey)
+	if err != nil {
+		log.Fatal("Error generating private key")
 	}
+
 	//append to data/server.properties file
 	file, err := os.OpenFile("data/server.properties", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 600)
 	if err != nil {
 		log.Fatalln("Error while generating the private key: ", err)
 	}
-	_, err = file.WriteString("\nprivateKey=" + privateKey)
+	_, err = file.WriteString("\nprivateKey=" + string(privateKey))
 	if err != nil {
 		log.Fatalln("Error while generating the private key: ", err)
 	}
 
-	_, _, err = props.Set("privateKey", privateKey)
+	_, _, err = props.Set("privateKey", string(privateKey))
 	if err != nil {
 		log.Fatalln("Failed to generate private key, try to restart: \n", err)
 	}
